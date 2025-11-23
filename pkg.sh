29ef31413b8444b8f4fbdc7d3a116e909f32cf07
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-11-23 09:18:56 +0800

    update: 修改 DMG 包镜像名称

--- pkg.sh
+++ pkg.sh
@@ -7,7 +7,8 @@ OUT_DIR=output
 OS_NAME=$(uname -s)
 OS_ARCH=$(uname -m)
 DMG_DIR=${OUT_DIR}/dmg
-DMG_IMG=${OUT_DIR}/${APP_PKG}-${OS_NAME}-${OS_ARCH}-${GIT_TAG}.dmg
+DMG_VOL=${APP_PKG}-${OS_NAME}-${OS_ARCH}-${GIT_TAG}
+DMG_IMG=${OUT_DIR}/${DMG_VOL}.dmg
 
 # Clean old builds
 rm -rf ${OUT_DIR}
@@ -19,4 +20,5 @@ uv run python -m nuitka app.py
 # Create dmg volume
 mkdir -p ${DMG_DIR}
 mv ${OUT_DIR}/${APP_PKG}.app ${DMG_DIR}
-hdiutil create -volname ${APP_PKG} -srcfolder ${DMG_DIR} -ov -format UDZO ${DMG_IMG}
+ln -s /Applications ${DMG_DIR}/Applications
+hdiutil create -volname ${DMG_VOL} -srcfolder ${DMG_DIR} -ov -format UDZO ${DMG_IMG}
